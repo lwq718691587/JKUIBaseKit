88d433c3bb901fd0ae5b197d7f391f9c2066af78
A <941636007@qq.com>
2017-05-25 18:50:13 +0800

    UI 类别

---
+++ JKUIBaseKit/UISearchBar+style.h
@@ -0,0 +1,16 @@
+//
+//  UISearchBar+style.h
+//  JianKeDoctor
+//
+//  Created by 刘伟强 on 2017/3/16.
+//  Copyright © 2017年 lijiawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISearchBar (style)
+
+-(void)jkStyle;
+// 是否居中
+-(void)setHasCentredPlaceholder:(BOOL)hasCentredPlaceholder;
+@end
---
+++ JKUIBaseKit/UISearchBar+style.m
@@ -0,0 +1,45 @@
+//
+//  UISearchBar+style.m
+//  JianKeDoctor
+//
+//  Created by 刘伟强 on 2017/3/16.
+//  Copyright © 2017年 lijiawei. All rights reserved.
+//
+
+#import "UISearchBar+style.h"
+
+@implementation UISearchBar (style)
+
+-(void)jkStyle{
+    [self setImage:[UIImage imageNamed:@"icon_input_search"] forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    [self setImage:[UIImage imageNamed:@"btn_input_erase"] forSearchBarIcon:UISearchBarIconClear state:UIControlStateNormal];
+    [self setImage:[UIImage imageNamed:@"btn_input_erase"] forSearchBarIcon:UISearchBarIconClear state:UIControlStateHighlighted];
+    
+    [self setBarStyle:UIBarStyleDefault];
+    [self setBackgroundColor:[UIColor clearColor]];
+    [[[self.subviews objectAtIndex:0].subviews objectAtIndex:0] removeFromSuperview];
+    UITextField *searchFiled = [self valueForKey:@"_searchField"];
+    searchFiled.layer.cornerRadius = 15;
+    searchFiled.layer.masksToBounds = YES;
+    //searchFiled.backgroundColor = HexColor(@"f5f5f5");
+    
+}
+
+- (void)setHasCentredPlaceholder:(BOOL)hasCentredPlaceholder
+{
+    
+    SEL centerSelector = NSSelectorFromString([NSString stringWithFormat:@"%@%@", @"setCenter", @"Placeholder:"]);
+    if ([self respondsToSelector:centerSelector])
+    {
+        NSMethodSignature *signature = [[UISearchBar class] instanceMethodSignatureForSelector:centerSelector];
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+        [invocation setTarget:self];
+        [invocation setSelector:centerSelector];
+        [invocation setArgument:&hasCentredPlaceholder atIndex:2];
+        [invocation invoke];
+    }
+    
+}
+
+
+@end
