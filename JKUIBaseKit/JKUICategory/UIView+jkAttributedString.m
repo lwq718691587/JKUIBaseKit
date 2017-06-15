908186e449eeacabf18a1fa4d7e4adace2cb8152
A <941636007@qq.com>
2017-06-15 17:50:43 +0800

    修改 UIView+NSAttribute分类的BUG

--- JKUIBaseKit/JKUICategory/UIView+jkAttributedString.m
+++ JKUIBaseKit/JKUICategory/UIView+jkAttributedString.m
@@ -16,22 +16,21 @@
 
 - (void) rangeText:(NSRange) range font:(UIFont *) font color: (UIColor *) color {
     
-    NSMutableAttributedString *attributedString = [self la_getAttributedString];
-    
-    if (font == nil || font.pointSize == 0) {
-        font = [self la_getAttributedFont];
-    }
-    
-    [attributedString addAttribute: NSFontAttributeName
-                             value: font
-                             range: range.length  == 0 ? NSMakeRange(0, attributedString.length) : range];
-    
-    if (color != nil) {
-        [attributedString addAttribute: NSForegroundColorAttributeName
-                                 value: color
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithAttributedString: [self la_getAttributedString]];
+    {
+        if (font == nil || font.pointSize == 0) font = [self la_getAttributedFont];
+        
+        [attributedString addAttribute: NSFontAttributeName
+                                 value: font
                                  range: range.length  == 0 ? NSMakeRange(0, attributedString.length) : range];
+        
+        if (color != nil) {
+            [attributedString addAttribute: NSForegroundColorAttributeName
+                                     value: color
+                                     range: range.length  == 0 ? NSMakeRange(0, attributedString.length) : range];
+        }
     }
-    
+    [self setValue:attributedString forKey:@"attributedText"];
 }
 
 - (void) rangeTextSpace:(CGFloat) space {
@@ -39,24 +38,29 @@
 }
 
 - (void) rangeText:(NSRange)range space:(CGFloat)space {
-    NSMutableAttributedString *attributedString = [self la_getAttributedString];
-    
-    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-    paragraphStyle.paragraphSpacing = space;
-    paragraphStyle.alignment        = NSTextAlignmentJustified;
-    
-    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:range.length  == 0 ? NSMakeRange(0, attributedString.length) : range];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithAttributedString: [self la_getAttributedString]];
+    {
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+        paragraphStyle.paragraphSpacing = space;
+        paragraphStyle.alignment        = NSTextAlignmentJustified;
+        
+        [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:range.length  == 0 ? NSMakeRange(0, attributedString.length) : range];
+    }
+    [self setValue:attributedString forKey:@"attributedText"];
 }
 
 - (void) rangeText:(NSRange)range image:(UIImage *) image imageRect:(CGRect) imageRect {
-    NSMutableAttributedString *attributedString = [self la_getAttributedString];
-    
-    NSTextAttachment *textAttachment = [[NSTextAttachment alloc] init];
-    textAttachment.image = image;
-    textAttachment.bounds = imageRect;
-    
-    [attributedString insertAttributedString:[NSAttributedString attributedStringWithAttachment:textAttachment ] atIndex:range.location];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithAttributedString: [self la_getAttributedString]];
+    {
+        NSTextAttachment *textAttachment = [[NSTextAttachment alloc] init];
+        textAttachment.image = image;
+        textAttachment.bounds = imageRect;
+        [attributedString insertAttributedString:[NSAttributedString attributedStringWithAttachment:textAttachment ] atIndex:range.location];
+    }
+    [self setValue:attributedString forKey:@"attributedText"];
 }
+
+
 
 #pragma mark - Private Function
 
